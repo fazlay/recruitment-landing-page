d6d3e0e8288b697e1fe32a5cab5b9166f4a56a44
fazlay <rfazlay21@gmail.com>
2023-08-03 00:58:43 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import GlobalStyles from 'styles/GlobalStyles';
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AgencyLandingPage from "MainPage/AgencyLandingPage";
+
+
+export default function App() {
+
+
+  return (
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AgencyLandingPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
